57295b518412df4bf5fc751cbabc3b40124af4cb
nk.yun <nagy123@naver.com>
2024-10-20 23:44:46 +0900

    feat : apply splash page design

--- lib/presentation/pages/splash/splash_page.dart
+++ lib/presentation/pages/splash/splash_page.dart
@@ -1,35 +1,37 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../routes/route_info.dart';
 
-class SplashPage extends StatelessWidget {
+// when this page exposes,
+// check the list below
+// - login check
+// - authentication check
+// - nertwork check
+
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () => context.go(RouteInfo.home.path));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('splash'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'splash page',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () => context.push(RouteInfo.home.path),
-              child: Text('move home'),
-            ),
-          ],
-        ),
-      ),
+      body: Center(child: SvgPicture.asset('assets/svg/main_logo.svg')),
+      // TODO 하드코딩 된 값 변경할 것
+      backgroundColor: Color(0xFF5f0080),
     );
   }
 }
