ab1488ae8adac6e48ef0c29997d8fe916ea7c428
윤남경 <nk.yun@bpmg.biz>
2024-10-21 19:06:33 +0900

    chore : init firebase console setting

---
+++ lib/core/fcm/fcm_manager.dart
@@ -0,0 +1,12 @@
+// init firebase cloud messaging
+// 1. connect firebase console, if you don't have firebase console project, you create project first.
+// 2. set up firebaase CLI at your dev environment.
+// 3. connect firebase login
+// 4. active flutterfire_cli :: command :: $ dart pub global activate flutterfire_cli
+// 5. automative enrollment your app to firebase and create lib/firbase_options.dart :: command :: flutterfire configure --project=e-commerce-app-81dd5 //firebase project id
+// * flutterfire is comfortable package setup of firebase
+// 6. set up android application id :: com.dev.e_commerce_app
+// * Platform  Firebase App Id
+// android   1:397186320225:android:e801e9c7190f72a7192720
+// ios       1:397186320225:ios:4a17d891a8c37366192720
+class FcmManager {}
