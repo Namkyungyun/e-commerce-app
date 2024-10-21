da23e03bdc14032071574b8c483e1f0041a32667
nk.yun <nagy123@naver.com>
2024-10-21 23:47:07 +0900

    chore : ios pod update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
-import 'presentation/pages/home/home_page.dart';
-import 'presentation/pages/splash/splash_page.dart';
 import 'presentation/routes/routes.dart';
 
 void main() {
